cad6115833179c890bee7a245d8f36891f1236e7
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-12 13:39:53 -0500

    know how to use node-persist

--- passwordmanager/app.js
+++ passwordmanager/app.js
@@ -1,1 +1,25 @@
 console.log('Starting password manager!');
+
+var storage = require('node-persist');
+storage.initSync();
+
+/*storage.setItemSync('accounts', [{
+  username: 'yoyo',
+  balance: 0
+}]);
+*/
+
+var accounts = storage.getItemSync('accounts');
+
+//push on a new account
+/*
+accounts.push({
+  username: 'Chen',
+  balance: 75
+});
+*/
+
+//save using setItemSync
+//storage.setItemSync('accounts', accounts);
+
+console.log(accounts);
