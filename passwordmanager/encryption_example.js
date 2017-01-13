c55251cfd03f3f58336fe616db0945eded9f67bb
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-13 00:56:07 -0500

    notice the crypto.AES.encrypt first variable need string mode

--- passwordmanager/encryption_example.js
+++ passwordmanager/encryption_example.js
@@ -1,12 +1,16 @@
 var crypto = require('crypto-js');
 
-var secretMessage = 'I hid the chips under the couch.';
+var secretMessage = {
+  name: 'Yoyo',
+  secretName: 'lover'
+};
 var secretKey = '123abc';
 
-var encryptedMessage = crypto.AES.encrypt(secretMessage, secretKey);
+var encryptedMessage = crypto.AES.encrypt(JSON.stringify(secretMessage), secretKey);
 console.log('Encrypted Message: ', encryptedMessage);
 
 var byte = crypto.AES.decrypt(encryptedMessage, secretKey);
-var decryptedMessage = byte.toString(crypto.enc.Utf8);
+var decryptedMessage = JSON.parse(byte.toString(crypto.enc.Utf8));
 
 console.log('decrypted message: ' + decryptedMessage);
+console.log(decryptedMessage.secretName);
