18389f64acef7dacebbb4eeec2ef2146cf0970ec
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-12 19:59:56 -0500

    input with module yargs

--- passwordmanager/app.js
+++ passwordmanager/app.js
@@ -9,7 +9,7 @@ storage.initSync();
 }]);
 */
 
-var accounts = storage.getItemSync('accounts');
+//var accounts = storage.getItemSync('accounts');
 
 //push on a new account
 /*
@@ -22,4 +22,49 @@ accounts.push({
 //save using setItemSync
 //storage.setItemSync('accounts', accounts);
 
-console.log(accounts);
+//console.log(accounts);
+
+//account.name Facebook
+//account.username User12!
+//account.password password123!
+function createAccount (account) {
+  var accounts = storage.getItemSync('accounts');
+
+  //if accounts is undefined (use typeof)
+  //  set accounts = [];
+  if (typeof accounts === 'undefined') {
+    accounts = [];
+  }
+  //push on a new account
+  accounts.push(account);
+
+  storage.setItemSync('accounts', accounts);
+  //return account
+  return account;
+}
+
+function getAccount (accountName) {
+  //var accounts use getItemSync
+  var accounts = storage.getItemSync('accounts');
+  //iterate over array, return atching account, else undefined
+  var matchedAccount;
+
+  accounts.forEach(function (account) {
+    if (account.name === accountName) {
+      matchedAccount = account;
+    }
+  });
+
+  return matchedAccount;
+}
+
+/*
+createAccount({
+  name: 'Facebook',
+  username: 'someemail@gmail.com',
+  password: 'Password123!'
+});
+*/
+
+var facebookAccount = getAccount('Facebook');
+console.log(facebookAccount);
