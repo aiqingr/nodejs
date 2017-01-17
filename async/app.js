0b58a3c7889efe27c37e84f86d996f2d6885b46a
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-17 17:54:40 -0500

    replace all callback with promise to make the code much easier

--- async/app.js
+++ async/app.js
@@ -13,18 +13,30 @@ var argv = require('yargs')
 
 if (typeof argv.l === 'string' && argv.l.length > 0) {
   console.log('Location was provided.');
-  weather(argv.l, function (currentWeather) {
+  // weather(argv.l, function (currentWeather) {
+  //   console.log(currentWeather);
+  // });
+  weather(argv.l).then(function (currentWeather) {
     console.log(currentWeather);
+  }).catch(function (error) {
+    console.log(error);
   });
 } else {
   console.log('Location was not provided.');
-  location(function (location) {
-    if (location) {
-      weather(location.city, function (currentWeather) {
-        console.log(currentWeather);
-      });
-    } else {
-      console.log('Unable to guess location');
-    }
-  });
+  // location(function (location) {
+  //   if (location) {
+  //     weather(location.city, function (currentWeather) {
+  //       console.log(currentWeather);
+  //     });
+  //   } else {
+  //     console.log('Unable to guess location');
+  //   }
+  // });
+  location().then(function (loc) {
+    return weather(loc.city);
+  }).then(function (currentWeather) {
+    console.log(currentWeather);
+  }).catch(function (error) {
+    console.log(error);
+  })
 }
--- async/weather.js
+++ async/weather.js
@@ -1,21 +1,23 @@
 var request = require('request');
 
-module.exports = function (location, callback) {
-  var encodedLocation = encodeURIComponent(location);
-  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodedLocation +'&units=imperial&appid=6c80a288b705cbb66950ba2a26a9b5e3';
+module.exports = function (location) {
+  return new Promise(function (resolve, reject) {
+    var encodedLocation = encodeURIComponent(location);
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodedLocation +'&units=imperial&appid=6c80a288b705cbb66950ba2a26a9b5e3';
 
-  if(!location) {
-    return callback('No location provided');
-  }
-
-  request({
-    url: url,
-    json: true
-  }, function (error, response, body) {
-    if (error) {
-      callback('Unable to fetch weather.');
-    } else {
-      callback(body.name + ' is ' + body.main.temp);
+    if(!location) {
+      return reject('No location provided');
     }
+
+    request({
+      url: url,
+      json: true
+    }, function (error, response, body) {
+      if (error) {
+        reject('Unable to fetch weather.');
+      } else {
+        resolve(body.name + ' is ' + body.main.temp);
+      }
+    });
   });
 }
