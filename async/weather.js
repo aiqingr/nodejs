79d234f42f6d5a56d01ec6f26741b9bce85c8942
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-16 23:28:17 -0500

    how to set different module in different files and require them in the mian javascript file

--- async/weather.js
+++ async/weather.js
@@ -1,3 +1,17 @@
-module.exports = function () {
-  console.log('Got weather.');
+var request = require('request');
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=Wuhu&units=imperial&appid=6c80a288b705cbb66950ba2a26a9b5e3';
+
+module.exports = function (callback) {
+  request({
+    url: url,
+    json: true
+  }, function (error, response, body) {
+    if (error) {
+      callback('Unable to fetch weather.');
+    } else {
+      //console.log(JSON.stringify(body, null, 4));
+      // It's 28.4 in Beijing
+      callback(body.name + ' is ' + body.main.temp);
+    }
+  });
 }
