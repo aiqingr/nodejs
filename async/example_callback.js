194f0fd5446a021b12e7ad46ccff6dfb5db473fa
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-16 23:08:09 -0500

    how to require other files in a javascript file

--- async/example_callback.js
+++ async/example_callback.js
@@ -1,17 +1,18 @@
 var request = require('request');
-var url = 'http://api.openweathermap.org/data/2.5/weather?q=Beijing&units=imperial&appid=6c80a288b705cbb66950ba2a26a9b5e3';
-
-request({
-  url: url,
-  json: true
-}, function (error, response, body) {
-  if (error) {
-    console.log('Unable to fetch weather.');
-  } else {
-    console.log(JSON.stringify(body, null, 4));
-    // It's 28.4 in Beijing
-    console.log(body.name + ' is ' + body.main.temp);
-  }
-});
-
-console.log('After the request');
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=Wuhu&units=imperial&appid=6c80a288b705cbb66950ba2a26a9b5e3';
+var weather = require('./weather.js')
+weather();
+// request({
+//   url: url,
+//   json: true
+// }, function (error, response, body) {
+//   if (error) {
+//     console.log('Unable to fetch weather.');
+//   } else {
+//     console.log(JSON.stringify(body, null, 4));
+//     // It's 28.4 in Beijing
+//     console.log(body.name + ' is ' + body.main.temp);
+//   }
+// });
+//
+// console.log('After the request');
