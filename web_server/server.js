0696babb62e4248c532e925bdced8838be2525cf
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-18 00:01:11 -0500

    finger out what is middleware

--- web_server/server.js
+++ web_server/server.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var app = express();
-var port = 3010
+var port = 3010;
+
+var middleware = {
+  requireAuthentication: function (req, res, next) {
+    console.log('private route hit!');
+    next();
+  },
+  logger: function (req, res, next) {
+    //new Date().toStirng()
+
+    console.log('Request: ' + new Date().toString() + req.method + ' ' + req.originalUrl);
+    next();
+  }
+};
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
 
 //about
-app.get('/about', function (req, res) {
+app.get('/about', middleware.requireAuthentication, function (req, res) {
   res.send('About us');
 });
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function () {
-  console.log('Express server' + port + 'started!');
+  console.log('Express server ' + port + ' started!');
 });
